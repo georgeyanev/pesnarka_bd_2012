b4b543a3744d250d91bd36e93d96939bb54b5bae
George <george.yanef@gmail.com>
2021-09-22 20:26:18 +0300

    Fix build script

--- build/make_all.sh
+++ build/make_all.sh
@@ -3,7 +3,7 @@
 cat ../016_zorata_na_noviya_zhivot.ly \
     ../018_izlyazal_e_seyach.ly \
     ../020_bratstvo_edinstvo.ly \
-    ../021_ste_se_razveselya.ly \
+    ../021_shte_se_razveselya.ly \
     ../022_izgryava_den_tarzhestven.ly \
     ../024_blagoslavyay.ly \
     ../025_sabudi_se_bratko.ly \
@@ -19,17 +19,15 @@ cat ../016_zorata_na_noviya_zhivot.ly \
     ../039_sardechen_zov.ly \
     ../040_blagosloven_Gospod.ly \
     ../041_za_nebesniya_tsar.ly \
-    ../42_vreme_e_da_varvim.ly \
+    ../042_vreme_e_da_varvim.ly \
     ../044_pozdrav_na_Uchitelya.ly \
     ../045_sine_moy_pazi_zhivota.ly \
     ../046_rosna_kapka.ly \
-    ../047_аz_sam_byaloto_kokiche.ly \
-    ../048_napred_chada_napred \
-    ../049_slaveycheta_gorski \
-    ../050_nebeto_se_otvarya_lyrics_de \
-    ../051_o_Uchitelyu_blagati_lyric_de \
-
-
+    ../047_az_sam_byaloto_kokiche.ly \
+    ../048_napred_chada_napred.ly \
+    ../049_slaveycheta_gorski.ly \
+    ../050_nebeto_se_otvarya.ly \
+    ../051_o_Uchitelyu_blagati.ly \
     > tmp.ly
 
 lilypond --include=../ -o pesn_all tmp.ly
